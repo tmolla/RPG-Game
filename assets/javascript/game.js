8424461fdda2143e7c6b7ca72acc0e12c4d4642a
Telahoun Molla <telahounmolla@gmail.com>
2019-02-23 02:15:28 -0700

    Save changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,23 @@
-var images = []
-images[0] = "Yoda.jpeg";
-images[1] = "Luke_Sky_walker.jpeg";
-images[2] = "Darth_Vader2.jpeg";
-images[3] = "Bobo_Fett.jpeg";
-images[4] = "FireMan.jpeg";
-images[5] = "Kylo_Ren.Jpeg";
-images[6] = "Luke_Sky_Walkerjpeg";
-images[7] = "Han_Solo.jpeg";
-images[8] = "Obi_One_Kenobi.jpeg";
-images[8] = "Mace_Windu.jpeg";
+var defender = "";
+var you = "";
+var caracters = [ 
+    {name: "Master Yoda", healthPoint:200, attackPoints:50, image:"Yoda.jpeg"},
+    {name: "Mace Windu", healthPoint:175, attackPoints:45, image:"Mace_Windu.jpeg"},
+    {name: "Luke Skywalker", healthPoint:130, attackPoints:30, image:"Luke_Sky_walker.jpeg"},
+    {name: "Kylo Ren", healthPoint:125, attackPoints:40, image:"Kylo_Ren.Jpeg"},
+    
+]
+
 function setUp(){
+    for (var i=0; i<caracters.length; i++){
+        caracters[i].healthPoint = Math.floor(Math.random() * 51) + 100
+        caracters[i].attackPoints = Math.floor(Math.random() * 5) + 5;
+    }
     var k = 0
     $('.char').each(function(){
-        $(this).attr({"src":"assets/images/" + images[k], "height":"75px", "width":"100px"});
-        $(this).parent().find(".name").text("yea " + k);
-        $(this).parent().find(".healthpoint").text(100 + k);
+        $(this).attr({"src":"assets/images/" + caracters[k].image, "data-index":k, "height":"75px", "width":"100px"});
+        $(this).parent().find(".name").html("<small>"+caracters[k].name)+"</small>";
+        $(this).parent().find(".healthpoint").html("<small>"+caracters[k].healthPoint+"</small>");
         k++;
     }) // Find every cell containing "Henry"
 
@@ -24,35 +27,40 @@ function setUp(){
  
 }
     
+$(document).on("click", "#attack", function(){
+    var dHP = $(".defed").attr("data-index");
+    var mHP = $(".me").attr("data-index");
+    caracters[dHP].healthPoint -= caracters[mHP].attackPoints;
+    caracters[mHP].healthPoint -= caracters[dHP].attackPoints;
+    caracters[mHP].attackPoints *= 2; 
+    $(".me").siblings(".healthpoint").html("<small>" + caracters[mHP].healthPoint + "</small>");
+    $(".defed").siblings(".healthpoint").html("<small>" + caracters[dHP].healthPoint + "</small>");
+    console.log("me hp = " + caracters[mHP].healthPoint)
+    console.log("def hp = " + caracters[dHP].healthPoint)
+    console.log("me ap = " + caracters[mHP].attackPoints)
 
+});
 $(document).ready(function(){
     setUp();
-    //get all images from the caracgers div
-    //set each one with images, name, health point and attack points
-    /* j = 0
-    $("#caracters").children().each(function(){
-        $(this).attr({"src":"assets/images/" + images[j], "height":"75px", "width":"100px"});
-        j++;
-    })
-    $(".tt").attr("src", "assets/images/Yoda.jpeg"); */
+
 });
 
 $(document).on("click", ".caracters", function(){
     alert("clicked on caracter");
     var enemies = $(this).siblings();
-    //$(enemies).removeClass()
-    //$("#caracters").remove($(this).siblings());
     $(".enemy-container").append($(enemies));
     $(".enemy-container").children().removeClass();
     $(".enemy-container").children().addClass("enemies");
     $(".enemies").css({"margin-right":"4px"});
     $("#you").append($(this));
+    $(this).find("img").removeClass().addClass("me");
+    console.log($(".me").attr("data-index"));
 });
 
-$(document).on("click", ".enemy", function(){
+$(document).on("click", ".enemies", function(){
     alert("clicked on enemy");
     //if ($("#defender").is(":empty")){
-        $(this).css({"id":"defed"});
+        $(this).find("img").removeClass().addClass("defed");
         $("#defender").prepend($(this));
     //}
 });
